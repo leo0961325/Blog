16d21816e974bcb7270f0a0e912850a2046adfad
leo <leo09613257@gmail.com>
2021-02-13 01:26:24 +0800

    add types function

--- src/main/java/com/blog/controller/admin/TypeController.java
+++ src/main/java/com/blog/controller/admin/TypeController.java
@@ -11,7 +11,9 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/admin")
@@ -24,7 +26,7 @@ public class TypeController {
     @GetMapping("/types")
     //Pageable選domin的
     //一頁10筆; 以Id做排序 ; direction並以降冪的方式呈現
-    public String types(@PageableDefault(size = 10,sort = {"id"},direction = Sort.Direction.DESC) Pageable pageable,
+    public String types(@PageableDefault(size = 3,sort = {"id"},direction = Sort.Direction.DESC) Pageable pageable,
                         Model model){
 
         //是Page物件
@@ -34,6 +36,31 @@ public class TypeController {
         return "admin/types";
     }
 
+    /**
+     * 返回新增"分類"頁面
+     * @return
+     */
+    @GetMapping("/types/input")
+    public String input(){
+
+        return "admin/types-input";
+    }
+
+    @PostMapping("/types")
+    public String post(Type type , RedirectAttributes attributes){
+
+        Type typeSaved = iTypeService.saveType(type);
+
+        if (typeSaved == null){
+            //操作消息顯示RedirectAttributes
+            attributes.addFlashAttribute("message","操作失敗！");
+        }
+        else {
+            attributes.addFlashAttribute("message","操作成功!!");
+        }
+        //重定向 -> 因為沒有經過樓上的types去查詢拿不到物件所以必須重定向
+        return "redirect:/admin/types";
+    }
 
 
 
